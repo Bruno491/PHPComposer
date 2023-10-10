7a2eae0f615518c29dc908112baf82d1d1a15397
Bruno Pereira da Silva <bruno.psilva56@outlook.com>
2023-10-10 09:22:19 -0300

    Initial commit

---
+++ bootstrap.php
@@ -0,0 +1,49 @@
+<?php
+
+    require __DIR__.'/vendor/autoload.php';
+
+    $method =  $_SERVER["REQUEST_METHOD"];
+    $path = $_SERVER['PATH_INFO'] ?? '/';
+
+    $router = new \Php\Biblioteca\Router($method, $path);
+
+    #$router->get("/", function(){ echo "Olá Mundo"; });
+
+    $router->get("/", 
+        'Php\Biblioteca\Controller\HomeController@index'
+    );
+
+    $router->get("/categoria/inserir", 
+        'Php\Biblioteca\Controller\CategoriaController@inserir'
+    );
+
+    $router->get("/categoria/alterar", 
+        'Php\Biblioteca\Controller\CategoriaController@alterar'
+    );
+
+    $router->get("/categoria/excluir", 
+        'Php\Biblioteca\Controller\CategoriaController@excluir'
+    );
+
+    $router->post("/categoria/gravar", 
+        'Php\Biblioteca\Controller\CategoriaController@gravar'
+    );
+
+    $router->get("/categoria/index", 
+        'Php\Biblioteca\Controller\CategoriaController@index'
+    );
+
+    $resultado = $router->handler();
+
+    if (!$resultado){
+        http_response_code(404);
+        echo "Página não encontrada";
+        die();
+    }
+
+    $resultado = explode('@', $resultado);
+    $controller = new $resultado[0];
+    $action = $resultado[1];
+    echo $controller->$action($router->getParams());
+
+?>
---
+++ src/View/Home/index.php
@@ -0,0 +1,7 @@
+<?php
+    require "../src/View/cabecalho.php";
+?>
+        <h1>CRUD PHP</h1>
+<?php
+    require "../src/View/rodape.php";
+?>
